135e766ba1043de7388090b2a7d35242ac91698b
Abdullah Mohammad <fordes12@gmail.com>
2020-12-16 22:09:15 -0600

    Changed Routes folder name to Capital

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Routes from '../src/components/routes/Routes';
+import Routes from './components/Routes/Routes';
 import { ThemeProvider } from '@material-ui/core/styles'
 import Theme from './theme/Theme'
 import './app.css'

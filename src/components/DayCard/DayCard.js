4ecca3cd6c8f3bb8444135967f70d43584cc12b9
Abdullah Mohammad <fordes12@gmail.com>
2020-12-18 23:03:32 -0600

    Added gradient color animation Added gradient color props refactored home state variable

--- src/components/DayCard/DayCard.js
+++ src/components/DayCard/DayCard.js
@@ -5,11 +5,29 @@ import './dayCard.css'
 
 const DayCard=(props)=> {
   const [displayDate, setDisplayDate]=useState(props.date);
+  const [gradientStyle, setGradientStyle]=useState({
+    backgroundImage: "linear-gradient(45deg,"+props.primaryColor+","+props.secondaryColor+")",
+    backgroundSize: "100% 100%"
+  });
+
+  const convertDateFormat=(argDate)=>{
+    let allMonths=[    
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"]
+    let month= allMonths[argDate.getMonth()];
+    let dayNumber= argDate.getDate();
+    let year=argDate.getFullYear();
+    return month+" "+dayNumber+", "+year;
+  }
+
+  console.log(gradientStyle.backgroundImage);
   return (
-        <div className="DayCard ">
-            <Paper elevation={6} className="dayCardHeight gradientBackgroud">
-                <Typography variant="h4">
-                    {displayDate.toString()}
+        <div className="DayCard">
+            <Paper elevation={6} className="dayCardHeight centerHorizontally dayCardPadding" style={gradientStyle}>
+                <Typography variant="h5" className="centerHorizontally">
+                    {convertDateFormat(displayDate)}
                 </Typography>
             </Paper>
         </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,17 +2,34 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import {useState} from 'react';
 import DayCard from '../DayCard/DayCard';
-import './home.css'
+import './home.css';
 
 const Home=()=> {
-  const[datesInView, setDatesInView]=useState([new Date(Date.now() - 864e5), new Date(), new Date(Date.now() + 864e5)]);
+  const[cardsInView, setCardsInView]=useState([
+    {
+      date:new Date(Date.now() - 864e5),
+      primaryColor:'#7b4397',
+      secondaryColor:'#dc2430'
+    },
+    {
+      date:new Date(),
+      primaryColor:'#5A3F37',
+      secondaryColor:'#2C7744'
+
+    },
+    {
+      date:new Date(Date.now() + 864e5),
+      primaryColor:'#D1913C',
+      secondaryColor:'#FFD194'
+    },
+  ]);
   return (
     <Paper elevation={0} className="Home fullHeight">
         <div className="centerVertically fullHeight">
         <Grid container spacing={0} className="spaceEvenly" >
-            {datesInView.map(day =>
-              <Grid item xs={12} lg={3} key={day}>
-                    <DayCard date={day}/>
+            {cardsInView.map(card =>
+              <Grid item xs={12} lg={3} key={card.date}>
+                    <DayCard date={card.date} primaryColor={card.primaryColor} secondaryColor={card.secondaryColor}/>
                </Grid>
             )}
         </Grid>

b8872ae1e31e3a783b6e2771b2f5d051f09e7997
Abdullah Mohammad <fordes12@gmail.com>
2021-03-15 09:32:32 -0500

    fixed layout for tast entry

--- src/components/TaskEntry/TaskEntry.js
+++ src/components/TaskEntry/TaskEntry.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, Fragment } from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -21,35 +21,44 @@ const TaskEntry = (props) => {
     setMenuOpen(event.currentTarget);
   };
   return (
-    <Grid item xs={12} lg={12} className="TaskEntry">
-      <Typography variant="body2" gutterBottom>
-        <Checkbox
-          className="noLeftPadding taskCheckBox"
-          checked={props.complete}
-          name="taskCompleteBox"
-          onChange={() =>
-            handleCheckboxChange(props.dayIndex, props.entryIndex)
-          }
-        />
-        {props.content}
-        <MoreVertRoundedIcon
-          className="menuIcon"
-          aria-controls="entry-menu"
-          onClick={openMenu}
-        />
-        <Menu
-          id="entry-menu"
-          anchorEl={menuOpen}
-          keepMounted
-          open={Boolean(menuOpen)}
-          onClose={closeMenu}>
-          <MenuItem>Delete</MenuItem>
-          <MenuItem>Edit</MenuItem>
-        </Menu>
-      </Typography>
+    <Fragment>
+      <Grid container spacing={0} className="TaskEntry">
+        <Grid item xs={1} lg={1} >
+          <Checkbox
+            className="noLeftPadding taskCheckBox"
+            checked={props.complete}
+            name="taskCompleteBox"
+            onChange={() =>
+              handleCheckboxChange(props.dayIndex, props.entryIndex)
+            }
+          />
+        </Grid>
 
-      <Divider />
-    </Grid>
+        <Grid item xs={10} lg={10} >
+          <Typography variant="body2" gutterBottom>
+            {props.content}
+          </Typography>
+        </Grid>
+        <Grid item xs={1} lg={1} >
+          <MoreVertRoundedIcon
+            className="menuIcon"
+            aria-controls="entry-menu"
+            onClick={openMenu}
+          />
+          <Menu
+            id="entry-menu"
+            anchorEl={menuOpen}
+            keepMounted
+            open={Boolean(menuOpen)}
+            onClose={closeMenu}>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Edit</MenuItem>
+          </Menu>
+        </Grid>
+      </Grid>
+
+      <Divider variant="middle" />
+    </Fragment>
   );
 };
 export default TaskEntry;

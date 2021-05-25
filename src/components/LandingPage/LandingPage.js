067d0ac87d53e73010175b90114743c0d8cd9f7d
Abdullah Mohammad <abdullahmohammad@Abdullahs-MacBook-Pro.local>
2021-05-24 22:13:13 -0500

    added basic layout for landing page added landingPageContent added LandingPageHeader

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,15 +4,19 @@ import Grid from "@material-ui/core/Grid";
 import { useState, useEffect, useRef } from "react";
 import "./landingPage.css";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import LandingPageHeader from "../LandingPageHeader/LandingPageHeader";
+import LandingContent from "../LandingPageContent/LandingPageContent";
 
 const LandingPage = () => {
   useEffect(() => {}, []);
 
   return (
     <Paper elevation={0} className="LandingPage noBorderRadius">
-      <Grid container xs={12} lg={12} className="centerHorizontally">
-        <Grid item xs={12} lg={9}>
-          <Logo width="200px" />
+      <LandingPageHeader />
+
+      <Grid xs={12} lg={11} className="centerHorizontally">
+        <Grid lg={4}>
+          <LandingContent />
         </Grid>
       </Grid>
     </Paper>

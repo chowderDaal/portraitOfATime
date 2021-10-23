4043c5c68736cfb407e2085cc85151ef3ab28ac3
Abdullah Mohammad <fordes12@Gmail.com>
2021-10-23 14:29:08 -0500

    added beautiful react dnd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Routes from "./components/Routes/Routes";
 import { ThemeProvider } from "@material-ui/core/styles";
+import { DragDropContext } from "react-beautiful-dnd";
 import Theme from "./theme/Theme";
 import "./app.css";
 
 const App = () => {
   return (
-    <ThemeProvider theme={Theme}>
-      <div className="App">
-        <Routes />
-      </div>
-    </ThemeProvider>
+    <DragDropContext>
+      <ThemeProvider theme={Theme}>
+        <div className="App">
+          <Routes />
+        </div>
+      </ThemeProvider>
+    </DragDropContext>
   );
 };
 
